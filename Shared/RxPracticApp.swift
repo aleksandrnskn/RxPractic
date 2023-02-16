fb89cfc9686410b04b35019186ac9abc45d3083a
Aleksandr Aniskin <aleksandr.nskn@yandex.ru>
2023-02-17 00:31:08 +0300

    Initial Commit

---
+++ Shared/RxPracticApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxPracticApp.swift
+//  Shared
+//
+//  Created by Aleksandr Aniskin on 07.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RxPracticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
